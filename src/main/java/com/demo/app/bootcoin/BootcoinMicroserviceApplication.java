3628edf1b52081e0e90905e7afc5ea6f3efb5160
Moises Molina <w7moises@gmail.com>
2022-05-05 10:24:33 -0500

    template added

---
+++ src/main/java/com/demo/app/bootcoin/BootcoinMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.app.bootcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcoinMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcoinMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/app/bootcoin/BootcoinMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.app.bootcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcoinMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

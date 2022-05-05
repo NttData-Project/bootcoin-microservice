e3f940f080f4be99ba9f653ab93c311d57a7e295
Moises Molina <w7moises@gmail.com>
2022-05-05 10:36:38 -0500

    redis and basic configuration

--- src/main/java/com/demo/app/bootcoin/BootcoinMicroserviceApplication.java
+++ src/main/java/com/demo/app/bootcoin/BootcoinMicroserviceApplication.java
@@ -1,9 +1,16 @@
 package com.demo.app.bootcoin;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+@EnableReactiveMongoAuditing
+@EnableReactiveMongoRepositories
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "APIs", version = "1.0", description = "Documentation APIs v1.0"))
 public class BootcoinMicroserviceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/demo/app/bootcoin/config/ValidatorConfiguration.java
@@ -0,0 +1,44 @@
+package com.demo.app.bootcoin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.time.Duration;
+
+@Configuration
+public class ValidatorConfiguration {
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Bean
+    public ReactiveRedisConnectionFactory lettuceConnectionFactory() {
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl().and()
+                .commandTimeout(Duration.ofSeconds(2))
+                .shutdownTimeout(Duration.ZERO)
+                .build();
+
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379), clientConfig);
+    }
+
+    @Bean
+    ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.string());
+    }
+}
